0985f095c59eb6988ee8ea5c01bd033bce9ca0f3
Scrumpy (Jay) <scrumpy@weeb.email>
2022-03-18 22:41:57 +0000

    Updating my extremely old and extremely bad code.

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import discord
-from discord import *
 from discord.ext.commands import Bot
 from discord.ext import commands
-from discord.ext import *
-from discord import *
-from discord.ext import tasks
 
-bot_token = "token"
-bot_prefix = "prefix"
+BOT_TOKEN = "token"
+BOT_PREFIX = "prefix"
 
-Client = discord.Client()
-bot = commands.Bot(command_prefix = bot_prefix, case_insensitive=True)
+bot = commands.Bot(command_prefix=BOT_PREFIX, case_insensitive=True, intents=discord.Intents.default())
 
 @bot.event
 async def on_ready():
@@ -18,27 +13,18 @@ async def on_ready():
 
 @commands.has_permissions(view_audit_log=True)
 @bot.command()
-async def sendsplash(ctx, channel_: discord.TextChannel=None, slots_=None, rank_: discord.Role=None, *, data="None"):
-      data = data.split(" | ")
-      if (channel_ == None) or (slots_ == None) or (rank_ == None) or (len(data) != 2):
-            await ctx.send("%s, invalid format. `%ssendsplash #channel slots @rank where it is | how to join it`\nExample: `%ssendsplash #updates 10 @humans ontop of the hill | DM me.`" % (ctx.author.mention, bot_prefix, bot_prefix))
-            return
-      await channel_.send("""
-**Where:** %s
-**Positions:** %s,
-**How to join:** %s,
-**%s**
-      """ % (data[0], slots_, data[1], rank_.mention))
-      await ctx.send("%s, your post in %s has been sent." % (ctx.author.mention, channel_.mention))
+async def sendsplash(ctx, channel: discord.TextChannel=None, slots=None, role: discord.Role=None, *, data=None):
+      data = data.split(" | ") or ["None"]
+      if (channel is None) or (slots is None) or (role is None) or (len(data) != 2):
+            return await ctx.send(f"{ctx.author.mention}, invalid command format. `{BOT_PREFIX}sendsplash #channel slots @role where it is | how to join it`\nExample: `{BOT_PREFIX}sendsplash #updates 10 @humans ontop of the hill | DM me.`")
+      
+      await channel.send(f"**Where:** {data[0]}\n**Positions:** {slots},\n**How to join:** {data[1]},\n**{role.mention}**")
+      return await ctx.send(f"{ctx.author.mention}, your post in {channel.mention} has been sent."))
 
 @bot.event
 async def on_command_error(ctx, error):
       if isinstance(error, commands.MissingPermissions):
-            string = ""
-            for permission in list(error.missing_perms):
-                string = str(string)+str(permission)+", "
-            await ctx.send("%s, you're lacking the following permission(s) `%s`." % (ctx.author.name, string[0:((len(string))-2)]))
-      else:
-            print(error)
+            return await ctx.send(f"{ctx.author.name}, you're lacking the following permission: `{list(error.missing_perms)[0]}`.")
+      raise error
 
-bot.run(bot_token)
+bot.run(BOT_TOKEN)
